c5ac18bb1f3a9f702f93295ca6a3f474c09c6c07
Hesen Peng <hesen.peng@gmail.com>
2016-10-13 04:43:51 -0700

    adding digits example

---
+++ digits-knn.py
@@ -0,0 +1,29 @@
+
+## 这里我们用K-近邻估计来判断手写数字的扫描图像来判断数字是多少。
+
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+
+## 导入数据
+digits = datasets.load_digits()
+X_digits = digits.data
+y_digits = digits.target
+
+n_samples = len(X_digits)
+
+## 拆分训练集和测试集
+X_train = X_digits[:.9 * n_samples]
+y_train = y_digits[:.9 * n_samples]
+X_test = X_digits[.9 * n_samples:]
+y_test = y_digits[.9 * n_samples:]
+
+model = KNeighborsClassifier()
+
+## 训练模型
+model.fit(X_train, y_train)
+
+## 进行预测
+prediction = model.predict(X_test)
+
+score = model.score(X_test, y_test)
+print(score) 
