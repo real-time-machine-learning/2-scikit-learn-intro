9e1b0dd0a726178db1e6cd17d2f9569729713df2
Hesen Peng <hesen@uber.com>
2018-08-11 23:07:48 -0700

    更正train test split

--- digits-knn.py
+++ digits-knn.py
@@ -1,29 +1,29 @@
-
-## 这里我们用K-近邻估计来判断手写数字的扫描图像来判断数字是多少。
+# 这里我们用K-近邻估计来判断手写数字的扫描图像来判断数字是多少。
 
 from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 
-## 导入数据
+# 导入数据
 digits = datasets.load_digits()
 X_digits = digits.data
 y_digits = digits.target
 
 n_samples = len(X_digits)
 
-## 拆分训练集和测试集
-X_train = X_digits[:.9 * n_samples]
-y_train = y_digits[:.9 * n_samples]
-X_test = X_digits[.9 * n_samples:]
-y_test = y_digits[.9 * n_samples:]
+# 拆分训练集和测试集
+X_train, X_test, y_train, y_test = train_test_split(
+    X_digits,
+    y_digits,
+    test_size=0.1)
 
 model = KNeighborsClassifier()
 
-## 训练模型
+# 训练模型
 model.fit(X_train, y_train)
 
-## 进行预测
+# 进行预测
 prediction = model.predict(X_test)
 
 score = model.score(X_test, y_test)
-print(score) 
+print(score)
--- digits-linear-regression.py
+++ digits-linear-regression.py
@@ -1,29 +1,33 @@
-
-## 这里我们用逻辑回归来判断手写数字的扫描图像来判断数字是多少。
+# 这里我们用逻辑回归来判断手写数字的扫描图像来判断数字是多少。
 
 from sklearn import datasets
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
-## 导入数据
+# 导入数据
 digits = datasets.load_digits()
 X_digits = digits.data
 y_digits = digits.target
 
 n_samples = len(X_digits)
 
-## 拆分训练集和测试集
-X_train = X_digits[:.9 * n_samples]
-y_train = y_digits[:.9 * n_samples]
-X_test = X_digits[.9 * n_samples:]
-y_test = y_digits[.9 * n_samples:]
+# 拆分训练集和测试集
+
+# 这里因为我们的测试集和训练集不存在时间先后关系，所以可以使用Scikit
+# Learn自带的 train_test_split 函数自动化拆分数据集
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X_digits,
+    y_digits,
+    test_size=0.1)
 
 model = LogisticRegression()
 
-## 训练模型
+# 训练模型
 model.fit(X_train, y_train)
 
-## 进行预测
+# 进行预测
 prediction = model.predict(X_test)
 
 score = model.score(X_test, y_test)
-print(score) 
+print(score)
